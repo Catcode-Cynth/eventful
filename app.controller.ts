032f2cd8e7b80cd9eef2a47dcf90f9c0dd9380a3
Cynthia Okechukwu <106869145+Catcode-Cynth@users.noreply.github.com>
2026-06-07 23:01:32 +0100

    Add files via upload

---
+++ app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getRoot() {
+    return {
+      message: '🚀 Eventful API is live!',
+      docs: '/api/docs',
+    };
+  }
+}
